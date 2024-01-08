3966c3ecc70dbaead6c5de01fa658870dcb44ae3
Zhehui Huang <zhehuihu@usc.edu>
2024-01-07 16:42:03 -0800

    Modify script

--- swarm_rl/runs/hybrid/baseline.py
+++ swarm_rl/runs/hybrid/baseline.py
@@ -18,5 +18,7 @@ QUAD_BASELINE_CLI_8 = (
     '--quads_neighbor_encoder_type=no_encoder --quads_neighbor_visible_num=2 '
     # Obstacles
     '--quads_use_obstacles=True --quads_obst_spawn_area 8 8 --quads_obst_density=0.2 --quads_obst_size=0.6 '
-    '--quads_obst_collision_reward=5.0 --quads_obstacle_obs_type=octomap --quads_use_downwash=True'
+    '--quads_obst_collision_reward=5.0 --quads_obstacle_obs_type=octomap --quads_use_downwash=True '
+    # Sample-Factory
+    '--heartbeat_reporting_interval=2000'
 )
