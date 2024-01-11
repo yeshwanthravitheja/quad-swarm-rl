125886b52f9f4341bd549862eeb7867abb314b8e
Zhehui Huang <zhehuihu@usc.edu>
2024-01-10 22:07:43 -0800

    Add runner scripts

--- train_brain.sh
+++ train_brain.sh
@@ -1,5 +1,5 @@
 python -m sample_factory.launcher.run \
---run=swarm_rl.runs.hybrid.quads_hybrid_search_no_sol_rl_acc \
+--run=swarm_rl.runs.hybrid.grid.quads_hybrid_v1 \
 --backend=slurm --slurm_workdir=slurm_output \
 --experiment_suffix=slurm --pause_between=1 \
 --slurm_gpus_per_job=1 --slurm_cpus_per_gpu=16 \
