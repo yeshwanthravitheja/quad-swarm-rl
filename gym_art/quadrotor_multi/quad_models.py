5b1c53fa9e2677b8ecd54e668ceb89072845a583
rahul-krupani <rahul.krupani@outlook.com>
2024-06-07 21:18:00 -0700

    Weight changes

--- gym_art/quadrotor_multi/quad_models.py
+++ gym_art/quadrotor_multi/quad_models.py
@@ -1,16 +1,27 @@
 def crazyflie_params():
+    # Mass of individual components (kg)
+    pcb_m = 0.0067
+    long_pin_m = 0.0007
+    propellor_m = 0.00035
+    motor_m = 0.00355 #Using thrust upgrade motors
+    battery_m = 0.00955
+    sd_card_m = 0.00225
+    flow_deck_m = 0.00167
+    tof_deck_m = 0.00738
+    arm_m = 0.0004
+
     ## See: Ref[2] for details
     ## Geometric parameters for Inertia and the model
     geom_params = {}
-    geom_params["body"] = {"l": 0.03, "w": 0.03, "h": 0.004, "m": 0.005}
-    geom_params["payload"] = {"l": 0.035, "w": 0.02, "h": 0.008, "m": 0.027}
-    geom_params["arms"] = {"l": 0.022, "w": 0.005, "h": 0.005, "m": 0.001}
-    geom_params["motors"] = {"h": 0.02, "r": 0.0035, "m": 0.0015}
-    geom_params["propellers"] = {"h": 0.002, "r": 0.022, "m": 0.00075}
+    geom_params["body"] = {"l": 0.03, "w": 0.03, "h": 0.02, "m": pcb_m + (2*long_pin_m) + battery_m + sd_card_m}
+    geom_params["payload"] = {"l": 0.035, "w": 0.045, "h": 0.015, "m": flow_deck_m + tof_deck_m}
+    geom_params["arms"] = {"l": 0.022, "w": 0.005, "h": 0.005, "m": arm_m * 4}
+    geom_params["motors"] = {"h": 0.02, "r": 0.0035, "m": motor_m * 4}
+    geom_params["propellers"] = {"h": 0.002, "r": 0.022, "m": propellor_m * 4}
 
     geom_params["motor_pos"] = {"xyz": [0.065 / 2, 0.065 / 2, 0.]}
     geom_params["arms_pos"] = {"angle": 45., "z": 0.}
-    geom_params["payload_pos"] = {"xy": [0., 0.], "z_sign": 1}
+    geom_params["payload_pos"] = {"xy": [0., 0.], "z_sign": -1}
     # z_sing corresponds to location (+1 - on top of the body, -1 - on the bottom of the body)
 
     ## Damping parameters
@@ -24,7 +35,7 @@ def crazyflie_params():
     ## Motor parameters
     motor_params = {"thrust_to_weight": 1.9,  #2.18
                     "assymetry": [1.0, 1.0, 1.0, 1.0],
-                    "torque_to_thrust": 0.006,  #0.005964552
+                    "torque_to_thrust": 0.005964552,  #0.006
                     "linearity": 1.0,  #0.424 for CrazyFlie w/o correction in firmware (See [2])
                     "C_drag": 0.000,  # 3052 * 9.1785e-07  #3052 * 8.06428e-05, # 0.246
                     "C_roll": 0.000,  #3052 * 0.000001 # 0.0003
