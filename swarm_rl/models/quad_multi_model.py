Incorrect units

swarm_rl/models/quad_multi_model.py
@@ -344,7 +344,7 @@ class QuadMultiEncoder(Encoder):
         # This is followed by another fully connected layer in the action parameterization, so we add a nonlinearity
         # here
         self.feed_forward = nn.Sequential(
-            fc_layer(total_encoder_out_size, 2 * cfg.rnn_size),
+            fc_layer(total_encoder_out_size, 2*cfg.rnn_size),
             nn.Tanh(),
         )
 