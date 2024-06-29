9ec69281617d4e03a9e7f663955f7731d16770b7
darren-chiu <darrenchiu321@gmail.com>
2024-06-29 16:46:34 -0700

    Fixed duration

--- gym_art/quadrotor_multi/scenarios/obstacles/o_random_dynamic_goal.py
+++ gym_art/quadrotor_multi/scenarios/obstacles/o_random_dynamic_goal.py
@@ -11,13 +11,10 @@ class Scenario_o_random_dynamic_goal(Scenario_o_base):
         super().__init__(quads_mode, envs, num_agents, room_dims)
         self.approch_goal_metric = 0.5
         self.goal_generator = []
-        # Episode duration will randomly sample from 4 to 15 seconds.
-        # self.duration_step = int(np.random.uniform(low=8, high=30) * self.envs[0].control_freq)
-        
-        # Fix the episode duration to 30 seconds total
-        self.duration_step = 60 * self.envs[0].control_freq
-
-        self.duration = self.duration_step*self.envs[0].dt
+ 
+        # Fix the episode duration to 15 seconds total
+        self.duration = 15
+        self.duration_step = self.duration / self.envs[0].dt
         
     def update_formation_size(self, new_formation_size):
         pass
@@ -34,13 +31,13 @@ class Scenario_o_random_dynamic_goal(Scenario_o_base):
             self.end_point[i] = next_goal_flat
             self.goals = copy.deepcopy(self.end_point)
             
-            if (self.goal_generator[i].plan_finished(time)):
+            # if (self.goal_generator[i].plan_finished(time)):
                 
-                final_goal = self.generate_pos_obst_map()
-                final_goal[2] = 0.65
+            #     final_goal = self.generate_pos_obst_map()
+            #     final_goal[2] = 0.65
                 
-                self.goal_generator[i].plan_go_to_from(initial_state=next_goal, desired_state=np.append(final_goal, np.random.uniform(low=0, high=3.14)), 
-                                                       duration=np.random.uniform(low=2, high=10), current_time=time)
+            #     self.goal_generator[i].plan_go_to_from(initial_state=next_goal, desired_state=np.append(final_goal, np.random.uniform(low=0, high=3.14)), 
+            #                                            duration=np.random.uniform(low=2, high=10), current_time=time)
             
         for i, env in enumerate(self.envs):
             env.goal = self.end_point[i]
@@ -78,7 +75,7 @@ class Scenario_o_random_dynamic_goal(Scenario_o_base):
             # Fix the goal height at 0.65 m
             final_goal[2] = 0.65
             self.goal_generator[i].plan_go_to_from(initial_state=initial_state, desired_state=np.append(final_goal, np.random.uniform(low=0, high=3.14)), 
-                                                   duration=np.random.uniform(low=2, high=10), current_time=0)
+                                                   duration=np.random.uniform(low=2, high=13), current_time=0)
             
             #Find the initial goal
             self.end_point.append(self.goal_generator[i].piecewise_eval(0).as_nparray())
