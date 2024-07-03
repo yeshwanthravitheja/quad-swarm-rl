d3aef7a6bf97cc083430087282aaa41703d4999a
darren-chiu <darrenchiu321@ghmail.com>
2024-07-03 11:04:39 -0700

    TODO: test initial scenarios

--- gym_art/quadrotor_multi/scenarios/obstacles/o_random_dynamic_goal_curriculum.py
+++ gym_art/quadrotor_multi/scenarios/obstacles/o_random_dynamic_goal_curriculum.py
@@ -9,12 +9,23 @@ from gym_art.quadrotor_multi.quadrotor_planner import traj_eval
 
 class Scenario_o_random_dynamic_goal(Scenario_o_base):
     """ This scenario implements a 13 dim goal that tracks a smooth polynomial trajectory. 
-        Each goal point is evaluated through the polynomial generated per reset."""
+        Each goal point is evaluated through the polynomial generated per reset. This specific
+        implementation increases the number of polynomial evaluations by the epoch of training."""
         
     def __init__(self, quads_mode, envs, num_agents, room_dims):
         super().__init__(quads_mode, envs, num_agents, room_dims)
         self.approch_goal_metric = 0.5
         self.goal_generator = []
+
+        # This value sets how many TOTAL goal points are to be evaluated during an episode. This does not include
+        # the initial hover start point.
+        self.goal_curriculum = 3.0
+
+        #Tracks the required time between shifts in goal.
+        self.goal_dt = []
+
+        #Tracks the current time before a goal is changed. Init to all zeros.
+        self.goal_time = [0] * self.num_agents
 
         
     def update_formation_size(self, new_formation_size):
@@ -28,11 +39,18 @@ class Scenario_o_random_dynamic_goal(Scenario_o_base):
         time = self.envs[0].sim_steps*tick*(self.envs[0].dt) #  Current time in seconds.
         
         for i in range(self.num_agents):
-            next_goal = self.goal_generator[i].piecewise_eval(time)
-    
-            self.end_point[i] = next_goal.as_nparray()
-            # print(time, self.end_point[i])
-            self.goals = copy.deepcopy(self.end_point)
+            self.goal_time += self.envs[0].sim_steps*self.envs[0].dt
+
+            #change goals if we are within 1 time step
+            if (abs(self.goal_time[i] - self.goal_dt[i]) < (self.envs[0].sim_steps*self.envs[0].dt)):
+
+                next_goal = self.goal_generator[i].piecewise_eval(time)
+        
+                self.end_point[i] = next_goal.as_nparray()
+
+                self.goals = copy.deepcopy(self.end_point)
+
+                self.goal_time[i] = 0
             
         for i, env in enumerate(self.envs):
             env.goal = self.end_point[i]
@@ -70,10 +88,14 @@ class Scenario_o_random_dynamic_goal(Scenario_o_base):
             
             # Fix the goal height at 0.65 m
             final_goal[2] = 0.65
+            traj_duration = np.random.uniform(low=2, high=self.envs[0].ep_time)
+
             # Generate trajectory with random time from (2, ep_time)
             self.goal_generator[i].plan_go_to_from(initial_state=initial_state, desired_state=np.append(final_goal, np.random.uniform(low=0, high=3.14)), 
-                                                   duration=np.random.uniform(low=2, high=self.envs[0].ep_time), current_time=0)
+                                                   duration=traj_duration, current_time=0)
             
+            self.goal_dt.append(traj_duration / self.goal_curriculum)
+
             #Find the initial goal
             self.end_point.append(self.goal_generator[i].piecewise_eval(0).as_nparray())
         
