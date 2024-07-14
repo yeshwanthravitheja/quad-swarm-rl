2ed1acbdb7a9b00c19f7aed7036ca50049e6862a
darren-chiu <darrenchiu321@ghmail.com>
2024-07-14 12:37:14 -0700

    viz fix

--- gym_art/quadrotor_multi/scenarios/obstacles/o_random_dynamic_goal.py
+++ gym_art/quadrotor_multi/scenarios/obstacles/o_random_dynamic_goal.py
@@ -80,6 +80,9 @@ class Scenario_o_random_dynamic_goal(Scenario_o_base):
         self.spawn_points = copy.deepcopy(self.start_point)
         
         self.goals = copy.deepcopy(self.end_point)
+
+        for i, env in enumerate(self.envs):
+            env.dynamic_goal = True
         
         
         
